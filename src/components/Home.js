a37fed1fa337413e7761063898d0d369dd3d0493
omerhastaken <130785896+omerhastaken@users.noreply.github.com>
2024-03-28 20:38:35 +0300

    modified:   src/components/Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,9 +12,8 @@ function Home() {
         rightBtnText="Existing Inventory"
         />
     <Section 
-    title="Model 3"
-    description="Order Online for Touchless Delivery"
-    backgroundImg="model-3.jpg"
+    title="METIN 2"
+    description="..."
     leftBtnText="Custom Order"
     rightBtnText="Existing Inventory"
     />
